5a36388620576bbb68415cdfe621b3cc049216c5
Yousef <ywalotaibi2010@gmail.com>
2026-03-18 15:33:06 +0300

    Refactor loading the menu to be categorized
    This gives the menu 3 sections, beverages, appetizers, and main dishes.
    It is easier for customers to find what they want this way.

--- src/menu.js
+++ src/menu.js
@@ -1,28 +1,42 @@
+import orangeJuice from './assets/orange-juice.jpeg';
+
 class MenuItem {
-    constructor(name, description, price, calories, image) {
+    constructor(name, description, price, calories, image, type) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.calories = calories;
         this.image = image;
+        this.type = type;
+        if(!menuItems[this]) {
+            menuItems.push(this);
+        };
     };
 };
-
-const menuItems = [
-];
+const menuItems = [];
 export function loadMenuTab() {
     const contentDiv = document.querySelector('#content');
     contentDiv.replaceChildren('');
-    const menuDiv = document.createElement('div');
     const header = document.createElement('h1');
     header.textContent = 'Menu';
-    const itemsDiv = document.createElement('div');
-    itemsDiv.id = 'menu-div';
+
+    const beveragesHeader = document.createElement('h2');
+    beveragesHeader.textContent = 'Beverages';
+    const beveragesDiv = document.createElement('div');
+
+    const appetizersHeader = document.createElement('h2');
+    appetizersHeader.textContent = 'Appetizers';
+    const appetizersDiv = document.createElement('div');
+
+    const mainDishesHeader = document.createElement('h2');
+    mainDishesHeader.textContent = 'Main Dishes';
+    const mainDishesDiv = document.createElement('div');
+
     menuItems.forEach(item => {
         const itemDiv = document.createElement('div');
         itemDiv.classList.add('food-div');
 
-        const itemName = document.createElement('h2');
+        const itemName = document.createElement('h3');
         itemName.classList.add('food-name');
         itemName.textContent = item.name;
 
@@ -39,7 +53,6 @@ export function loadMenuTab() {
         aboutDiv.appendChild(itemImage);
 
         const detailsDiv = document.createElement('div');
-        detailsDiv.classList.add('food-details');
 
         const priceSpan = document.createElement('span');
         priceSpan.textContent = `${item.price}$`;
@@ -49,13 +62,32 @@ export function loadMenuTab() {
 
         detailsDiv.appendChild(caloriesSpan);
         detailsDiv.appendChild(priceSpan);
-        
+
+        aboutDiv.appendChild(detailsDiv);
+
         itemDiv.appendChild(itemName);
         itemDiv.appendChild(aboutDiv);
         itemDiv.appendChild(detailsDiv);
-        itemsDiv.appendChild(itemDiv);
+        switch(item.type) {
+            case 'beverage':
+                beveragesDiv.appendChild(itemDiv);
+                break;
+            case 'appetizer':
+                appetizersDiv.appendChild(itemDiv);
+                break;
+            case 'main dish':
+                mainDishesDiv.appendChild(itemDiv);
+                break;
+        };
     });
-    menuDiv.appendChild(header);
-    menuDiv.appendChild(itemsDiv);
-    contentDiv.appendChild(menuDiv);
+    contentDiv.appendChild(header);
+
+    contentDiv.appendChild(beveragesHeader);
+    contentDiv.appendChild(beveragesDiv);
+
+    contentDiv.appendChild(appetizersHeader);
+    contentDiv.appendChild(appetizersDiv);
+
+    contentDiv.appendChild(mainDishesHeader);
+    contentDiv.appendChild(mainDishesDiv);
 };
